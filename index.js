373325da7a26079391110368fb23de70bc26d41e
benogit <benporter95@protonmail.com>
2021-03-11 14:33:35 +1000

    Done With Lab

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    callback()
+  }
+
+function returnsANamedFunction(){
+    return function receivesAFunction(){
+      console.log("function created")
+    }
+}
+
+function returnsAnAnonymousFunction(){
+  return function(){return "i am string"}
+}
